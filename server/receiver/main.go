513cc986f7820b05e938eacef8c93ec2f125c12f
mamedkar <192686326+mamedkar@users.noreply.github.com>
2026-04-25 16:10:22 +0100

    UDP port sequence validator

--- server/receiver/main.go
+++ server/receiver/main.go
@@ -4,29 +4,64 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
-func main() {
+type Event struct {
+	Port int
+}
+
+func startListener(events chan Event, port int) {
 	addr := net.UDPAddr{
-		Port: 9000,
+		Port: port,
 		IP:   net.ParseIP("0.0.0.0"),
 	}
 
 	conn, err := net.ListenUDP("udp", &addr)
 	if err != nil {
-		panic("server cannot listen")
+		log.Fatal("error port :", port, ":", err)
 	}
 	defer conn.Close()
 
 	buffer := make([]byte, 2000)
+	for {
+		_, _, err = conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Println("cannot reading :", err)
+			continue
+		}
+		events <- Event{Port: port}
+	}
+}
+
+func main() {
+	state := 0
+	events := make(chan Event)
+
+	go startListener(events, 7000)
+	go startListener(events, 9000)
+	go startListener(events, 8000)
 
 	for {
-		n, clientAddr, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			log.Println("cannot read from client")
+		select {
+		case e := <-events:
+			switch state {
+			case 0:
+				if e.Port == 7000 {
+					state = 1
+				}
+			case 1:
+				if e.Port == 9000 {
+					state = 2
+				}
+			case 2:
+				if e.Port == 8000 {
+					fmt.Println("ACTION SUCCESS")
+					state = 0
+				}
+			}
+		case <-time.After(5 * time.Second):
+			state = 0
 		}
-		fmt.Println("Message :", string(buffer[:n]))
-		fmt.Println("Client Address:", clientAddr)
 	}
-
 }
--- server/sender/main.go
+++ server/sender/main.go
@@ -3,22 +3,42 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 )
 
-func main() {
+func sendUDP(port int, msg string) {
 	addr := net.UDPAddr{
-		Port: 9000,
-		IP: net.ParseIP("127.0.0.1"),
+		Port: port,
+		IP:   net.ParseIP("127.0.0.1"),
 	}
 
 	conn, err := net.DialUDP("udp", nil, &addr)
 	if err != nil {
-		panic("cannot connect with localhost")
+		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	_, err = conn.Write([]byte("Hello world"))
+	_, err = conn.Write([]byte(msg))
 	if err != nil {
-		log.Println("cannot write to localhost")
+		log.Println("error sending:", err)
 	}
+}
+
+func main() {
+	log.Println("Start test sequence...")
+
+	sendUDP(7000, "step 1")
+	log.Println("sent 7000")
+
+	time.Sleep(1 * time.Second)
+
+	sendUDP(9000, "step 2")
+	log.Println("sent 9000")
+
+	time.Sleep(1 * time.Second)
+
+	sendUDP(8000, "step 3")
+	log.Println("sent 8000")
+
+	log.Println("Done")
 }
